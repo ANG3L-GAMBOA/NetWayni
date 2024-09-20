0a29cc1276406850773064fe07f4b8c1b8aaa44c
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2024-09-20 08:16:44 -0500

    regulaciones

--- PruebaE/obj/Debug/net8.0/PruebaE.AssemblyInfo.cs
+++ PruebaE/obj/Debug/net8.0/PruebaE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad92ff3673d3456cea5f514af69b81e88c91089")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
